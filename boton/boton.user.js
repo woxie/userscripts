7507b13d5b75e6778b17e997e4d59997991186a2
woxie <33608237+woxie@users.noreply.github.com>
2022-02-14 13:21:30 -0600

    Update boton.user.js

--- boton/boton.user.js
+++ boton/boton.user.js
@@ -3,8 +3,8 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://imperialtoys.myshopify.com/admin/orders/*
 // @grant       none
-// @version     1.0
-// @author      -
+// @version     1.1
+// @author      JE, Rafa
 // @description 6/2/2022, 17:01:55
 // @downloadURL https://raw.githubusercontent.com/woxie/userscripts/main/boton/boton.user.js
 // @updateURL https://raw.githubusercontent.com/woxie/userscripts/main/boton/boton.user.js
@@ -24,7 +24,7 @@ function action(observer) {
     observer.disconnect();
     // code
   
-cuadroderecha = document.querySelector("#AppFrameMain > div > div > div.Polaris-Page-Header_z4uwg.Polaris-Page-Header--hasNavigation_gfpa8.Polaris-Page-Header--hasActionMenu_1wx04.Polaris-Page-Header--mediumTitle_bfol6 > div:nth-child(2) > div.Polaris-Page-Header__RightAlign_1ok1p");
+cuadroderecha = document.querySelector("div.Polaris-Page-Header__Row_375v7:nth-child(2)");
   
 const boton = document.createElement("button");
 boton.innerHTML = "Copiar EI";
@@ -54,14 +54,14 @@ function copyToClipboard(text) {
     document.body.removeChild(dummy);
 }
 
-var datos = document.querySelector("#AppFrameMain > div > div > div.Polaris-Page__Content_xd1mk > div > section > div.yyeuw > div.Polaris-Card_yis1o > div:nth-child(2) > div:nth-child(3) > div.Polaris-Stack_32wu2.Polaris-Stack--vertical_uiuuj.Polaris-Stack--spacingTight_1o4d6 > div > div > div.Polaris-Stack__Item_yiyol.Polaris-Stack__Item--fill_vpuzt > div > div > p").innerHTML.split("<br>");
+var datos = document.querySelector("._1daZ2 > div:nth-child(1) > p:nth-child(1)").innerHTML.split("<br>");
 var nombre = datos[0];
 var direccionbreaks = '"'+ datos[1] + '\n' + datos[2] + '\n' + datos[3] + '\n' + datos[4] + '"';
 var direccionespacios = datos[1] + " " + datos[2] + " " + datos[3] + " " + datos[4];
-var numero = document.querySelector("#AppFrameMain > div > div > div.Polaris-Page__Content_xd1mk > div > section > div.yyeuw > div.Polaris-Card_yis1o > div:nth-child(2) > div:nth-child(3) > div.Polaris-Stack_32wu2.Polaris-Stack--vertical_uiuuj.Polaris-Stack--spacingTight_1o4d6 > div > div > div.Polaris-Stack__Item_yiyol.Polaris-Stack__Item--fill_vpuzt > div > div > p > span").innerText;
-var correo = document.querySelector("#AppFrameMain > div > div > div.Polaris-Page__Content_xd1mk > div > section > div.yyeuw > div.Polaris-Card_yis1o > div:nth-child(2) > div:nth-child(2) > div.Polaris-Stack_32wu2.Polaris-Stack--noWrap_vecks > div.Polaris-Stack__Item_yiyol.Polaris-Stack__Item--fill_vpuzt > div > p._3mARw > button > span > span").innerText;
-var ordenconhash = document.querySelector("#AppFrameMain > div > div > div.Polaris-Page-Header_z4uwg.Polaris-Page-Header--hasNavigation_gfpa8.Polaris-Page-Header--hasActionMenu_1wx04.Polaris-Page-Header--mediumTitle_bfol6 > div:nth-child(1) > div.Polaris-Page-Header__TitleWrapper_bejfn > div > div > div > h1").innerText;
-var ordensinhash = document.querySelector("#AppFrameMain > div > div > div.Polaris-Page-Header_z4uwg.Polaris-Page-Header--hasNavigation_gfpa8.Polaris-Page-Header--hasActionMenu_1wx04.Polaris-Page-Header--mediumTitle_bfol6 > div:nth-child(1) > div.Polaris-Page-Header__TitleWrapper_bejfn > div > div > div > h1").innerText.split('#').join('');
+var numero = document.querySelector("._1daZ2 > div:nth-child(1) > p:nth-child(1) > span:nth-child(6)").innerText;
+var correo = document.querySelector(".Polaris-Button--textAlignLeft_1yjwh > span:nth-child(1) > span:nth-child(1)").innerText;
+var ordenconhash = document.querySelector(".Polaris-Header-Title_2qj8j").innerText;
+var ordensinhash = document.querySelector(".Polaris-Header-Title_2qj8j").innerText.split('#').join('');
 
 var copiarei = nombre + '\t' + numero + '\t' + direccionbreaks + '\t' + correo + '\t' + ordensinhash;
 var copiarpc = nombre + '\t' + numero + '\t' + direccionbreaks + '\t' + correo;
