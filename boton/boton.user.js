aba6afe3713b0ece9dd58e3cfafc951d1f0016c2
woxie <33608237+woxie@users.noreply.github.com>
2022-02-28 11:38:58 -0600

    Update boton.user.js

--- boton/boton.user.js
+++ boton/boton.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://imperialtoys.myshopify.com/admin/orders/*
 // @grant       none
-// @version     1.8
+// @version     1.9
 // @author      JE, Rafa
 // @description Botones para copiar los elementos de una pedido y pegarlos en Excel
 // @downloadURL https://raw.githubusercontent.com/woxie/userscripts/main/boton/boton.user.js
